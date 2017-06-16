d36821ca6462a0b4409bad182108d1a9bca41552
Dawid netbook <dawid73@o2.pl>
2017-06-16 11:25:48 +0200

    wyczyszczenie projektu

--- src/PierwszaKlasa.java
+++ src/PierwszaKlasa.java
@@ -1,18 +1,10 @@
 
 public class PierwszaKlasa {
 
-
-
-
     public static void main(String[] args) {
 
         System.out.println("Hello GIT!! ");
 
-        System.out.println("test z drugiego komputera");
-
-        System.out.println("ggggsgsgsg");
-
-        System.out.println("tetst");
     }
 
 }
