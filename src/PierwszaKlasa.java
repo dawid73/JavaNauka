a5a4fd769d1855d84a3dc1da3917cfb8e9571f66
dawid73@o2.pl <salonmebli73>
2017-06-16 11:10:51 +0200

    test

--- src/PierwszaKlasa.java
+++ src/PierwszaKlasa.java
@@ -3,6 +3,10 @@
  */
 public class PierwszaKlasa {
     public static void main(String[] args) {
+
         System.out.println("Hello GIT!! ");
+
+        System.out.println("test z drugiego komputera");
     }
+
 }
