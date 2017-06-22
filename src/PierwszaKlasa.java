8fe71605044a4439fea6053cdb5226c4262bd3de
Dawid netbook <dawid73@o2.pl>
2017-06-22 21:32:00 +0200

    wyczyszczenie projektu

--- src/PierwszaKlasa.java
+++ src/PierwszaKlasa.java
@@ -1,9 +1,17 @@
+import java.util.*;
 
 public class PierwszaKlasa {
 
     public static void main(String[] args) {
 
         System.out.println("Hello GIT!! ");
+
+        Scanner klawiatura = new Scanner(System.in);
+        int a = klawiatura.nextInt();
+        int b = klawiatura.nextInt();
+        System.out.println(a+b);
+
+
 
 
     }
