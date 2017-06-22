45fc0bc636859432d1f7736b641cc74f8cd96fd0
Dawid netbook <dawid73@o2.pl>
2017-06-22 21:36:18 +0200

    dodanie bankomatów

---
+++ src/bankomat/Bankomat.java
@@ -0,0 +1,10 @@
+package bankomat;
+
+/**
+ * Created by Lenovo on 2017-06-22.
+ */
+public class Bankomat {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
